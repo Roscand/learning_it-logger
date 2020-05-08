d934cedcffb145130c5659152f8966dcc1428d9d
RoscandDesign <roscanddesign@gmail.com>
2020-05-08 23:41:41 +0400

    addTech action

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,9 +1,14 @@
 // dependencies //
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
+import PropTypes from "prop-types";
+
+// redux //
+import { connect } from "react-redux";
+import { addTech } from "../../actions/techActions";
 
 // AddTechModal component //
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
 
@@ -11,7 +16,14 @@ const AddTechModal = () => {
         if (firstName === "" || lastName === "") {
             M.toast({ html: "Please enter the first and last name" });
         } else {
-            console.log(firstName, lastName);
+            const newTech = {
+                firstName,
+                lastName,
+            };
+            addTech(newTech);
+
+            M.toast({ html: `${firstName} ${lastName} was added as a tech.` });
+
             setFirstName("");
             setLastName("");
         }
@@ -61,4 +73,8 @@ const AddTechModal = () => {
     );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+    addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
